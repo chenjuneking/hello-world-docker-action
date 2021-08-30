c930ea3f2081e7faa681572acb451ede1748fd5a
jacketchen <342115615@qq.com>
2021-08-30 18:28:11 +0800

    fix lint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
   },
-}
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+};
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,20 +1,20 @@
-import mysql from "mysql2/promise";
+import mysql from 'mysql2/promise';
 
-const password = process.env.DATABASE_PASSWORD || "123456";
+const password = process.env.DATABASE_PASSWORD || '123456';
 const port = process.env.MYSQL_PORT ? Number(process.env.MYSQL_PORT) : 3306;
 
-describe("Test Database", () => {
+describe('Test Database', () => {
   let connection: mysql.Connection;
 
   beforeAll(async () => {
     connection = await mysql.createConnection({
-      host: "localhost",
+      host: 'localhost',
       port,
-      user: "root",
+      user: 'root',
       password,
-      database: "test",
+      database: 'test',
     });
-    await connection.execute("DROP TABLE IF EXISTS `users`");
+    await connection.execute('DROP TABLE IF EXISTS `users`');
   });
 
   afterAll(async () => {
@@ -23,18 +23,18 @@ describe("Test Database", () => {
     }
   });
 
-  test("create table `users`", async () => {
+  test('create table `users`', async () => {
     const [tables_1] = await connection.execute(`SHOW TABLES`);
     expect((tables_1 as any).length).toEqual(0);
 
     await connection.execute(
-      "CREATE TABLE `users` (" +
-        "`id` INT(32) NOT NULL AUTO_INCREMENT," +
-        "`first_name` VARCHAR(32) NOT NULL," +
-        "`last_name` VARCHAR(32) NOT NULL," +
-        "`age` TINYINT(2) DEFAULT 1," +
-        "PRIMARY KEY (`id`)" +
-        ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4"
+      'CREATE TABLE `users` (' +
+        '`id` INT(32) NOT NULL AUTO_INCREMENT,' +
+        '`first_name` VARCHAR(32) NOT NULL,' +
+        '`last_name` VARCHAR(32) NOT NULL,' +
+        '`age` TINYINT(2) DEFAULT 1,' +
+        'PRIMARY KEY (`id`)' +
+        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4',
     );
     const [tables_2] = await connection.execute(`SHOW TABLES`);
     expect((tables_2 as any).length).toEqual(1);
