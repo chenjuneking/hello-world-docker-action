533849b1003039b2a6a9ecb987d41bff93412e01
jacketchen <342115615@qq.com>
2021-08-31 17:02:45 +0800

    test: v2

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM docker:stable
 
-COPY entrypoint.sh /entrypoint.sh
+COPY dist/entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 type mysql >/dev/null 2>&1 && sudo service mysql stop || echo "mysql not present."
 
+INPUT_VOLUMES=tests/fixtures:/etc/mysql/fixtures&tests/my.cnf:/etc/mysql/my.cnf
 commands="docker run"
 
 echo "volumes: $INPUT_VOLUMES"
@@ -13,32 +14,32 @@ if [ -n "$INPUT_VOLUMES" ]; then
   done
 fi
 
-if [ -n "$INPUT_MYSQLROOTPASSWORD" ]; then
-  echo "Root password not empty, use root superuser"
-  commands="$commands -e MYSQL_ROOT_PASSWORD=$INPUT_MYSQLROOTPASSWORD"
-elif [ -n "$INPUT_MYSQLUSER" ]; then
-  if [ -z "$INPUT_MYSQLPASSWORD" ]; then
-    echo "The mysql password must not be empty when mysql user specify."
-    exit 1
-  fi
+# if [ -n "$INPUT_MYSQLROOTPASSWORD" ]; then
+#   echo "Root password not empty, use root superuser"
+#   commands="$commands -e MYSQL_ROOT_PASSWORD=$INPUT_MYSQLROOTPASSWORD"
+# elif [ -n "$INPUT_MYSQLUSER" ]; then
+#   if [ -z "$INPUT_MYSQLPASSWORD" ]; then
+#     echo "The mysql password must not be empty when mysql user specify."
+#     exit 1
+#   fi
 
-  echo "Use specified user and password"
-  commands="$commands -e MYSQL_RANDOM_ROOT_PASSWORD=true -e MYSQL_USER=$INPUT_MYSQLUSER -e MYSQL_PASSWORD=$INPUT_MYSQLPASSWORD"
-else
-  echo "Both root password and superuser are empty, must contains one superuser"
-  exit 1
-fi
+#   echo "Use specified user and password"
+#   commands="$commands -e MYSQL_RANDOM_ROOT_PASSWORD=true -e MYSQL_USER=$INPUT_MYSQLUSER -e MYSQL_PASSWORD=$INPUT_MYSQLPASSWORD"
+# else
+#   echo "Both root password and superuser are empty, must contains one superuser"
+#   exit 1
+# fi
 
-if [ -n "$INPUT_MYSQLDATABASE" ]; then
-  echo "Use specified database"
-  commands="$commands -e MYSQL_DATABASE=$INPUT_MYSQLDATABASE"
-fi
+# if [ -n "$INPUT_MYSQLDATABASE" ]; then
+#   echo "Use specified database"
+#   commands="$commands -e MYSQL_DATABASE=$INPUT_MYSQLDATABASE"
+# fi
 
-commands="$commands -d -p $INPUT_HOSTPORT:$INPUT_CONTAINERPORT mysql:$INPUT_MYSQLVERSION --port=$INPUT_CONTAINERPORT"
-commands="$commands --character-set-server=$INPUT_CHARACTERSETSERVER --collation-server=$INPUT_COLLATIONSERVER"
+# commands="$commands -d -p $INPUT_HOSTPORT:$INPUT_CONTAINERPORT mysql:$INPUT_MYSQLVERSION --port=$INPUT_CONTAINERPORT"
+# commands="$commands --character-set-server=$INPUT_CHARACTERSETSERVER --collation-server=$INPUT_COLLATIONSERVER"
 
-sh -c "$commands"
+# sh -c "$commands"
 
-time=$(date)
-results="MySQL $INPUT_MYSQLVERSION set up on $time"
-echo "::set-output name=results::$results"
+# time=$(date)
+# results="MySQL $INPUT_MYSQLVERSION set up on $time"
+# echo "::set-output name=results::$results"
