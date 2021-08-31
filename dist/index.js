a5f1d7d814a11b3e19484846804ae01c8cecfc23
jacketchen <342115615@qq.com>
2021-08-31 17:23:35 +0800

    test: v2

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import * as core from '@actions/core';
 const hostPort = core.getInput('hostPort');
 const containerPort = core.getInput('containerPort');
@@ -56,6 +57,6 @@ function generateEntrypoint(commands) {
     content.push(`time=$(date)`);
     content.push(`MySQL set up on $time`);
     content.push(`::set-output name=results::$results`);
-    fs.writeFileSync('entrypoint.sh', content.join('\n'));
+    fs.writeFileSync(path.join(__dirname, 'entrypoint.sh'), content.join('\n'));
 }
 run();
